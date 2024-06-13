4ba7bc100b4d2fa00c2c937a90154115cbacf689
Olamide720 <olamidewasiu720@gmail.com>
2024-06-13 08:29:29 -0700

    update

---
+++ index.js
@@ -0,0 +1,38 @@
+
+// const h1 = document.createElement("h1")
+// h1.textContent = "This is an imperative way to program"
+// h1.className = "header"
+// document.getElementById("root").append(h1)
+
+// //jsx
+// ReactDOM.render(
+//     <div>
+//         <h1 className="header">this is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>,
+//     document.getElementById("root")
+// )
+
+// const navbar = (
+//     <nav>
+//         <h1>Simon's Bistro</h1>
+//         <ul>
+//             <li>Menu</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//         </ul>
+//     </nav>
+// )
+
+// ReactDOM.render(navbar, document.getElementById("root"))
+
+
+//how to add image
+
+const page = (
+    <div>
+        <img src="./react-logo.png"/>
+    </div>
+)
+
+ReactDOM.render(page, document.getElementById(root))
